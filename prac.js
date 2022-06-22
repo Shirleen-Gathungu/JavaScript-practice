52008ba965abbd278c87721257ccedc983634bc2
Shirleen19 <YOUR_EMAIL>
2022-06-22 10:28:09 +0300

    comparison operators and conditional statements

---
+++ prac.js
@@ -0,0 +1,100 @@
+// function message (){
+//     var words="Hello Baloney"
+//     console.log(words)
+// }
+// document.write("Naima,Are you sleepy?")
+
+// var word="Hello"
+// var name="Nuzy"
+// var say=word+""+name
+// function greet(){
+//     console.log(say)
+// }
+
+// greet()
+
+
+// function cook (){
+//     var ingred="onions"
+//     let another="carrot"
+//     let comb=ingred+" "+another
+    
+// console.log(typeof(ingred))
+// }
+// cook()
+
+// var b =undefined
+// console.log(b)
+
+
+// let line=[5,8,7,9,[5]];
+// // var see=array.length;
+// let line2=line[1][0]
+// console.log(line2);
+
+// let a ="chip";
+// let b=a.concat("see")
+
+// class Pupil{
+//     constructor(name){
+//         this.name=name;
+//     }
+// }
+
+// Pupil=new Pupil("Shirleen");
+// console.log(Pupil.name);
+// Pupil.prototype.age =34 ;
+// console.log(Pupil.age)
+
+// var q="34"
+// console.log(Number(q)+3);
+// console.log(typeof q)
+
+
+/*let v =4
+let l=4+"Hello"
+console.log(l)
+let r="f"
+console.log(Number(r))
+*/
+
+// let word="Tony"
+// if(word=="Jim"){
+//     console.log("Hello Jim")
+// }
+// else{
+//     console.log("Not known bwahahaha!")
+// }
+
+// let number=67;
+// let numberb=50;
+// let add=number+numberb
+// if(number==67 && add==67){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+// const n=90;
+// switch(true){
+//     case n>= 10:
+//         console.log("Yoh ya");
+//         break;
+//     case n==20:
+//     console.log("Yeheh")
+//         break;
+    
+//     default:
+//         console.log("Yap yap")
+//         break;
+
+// }
+// const g=40;
+// let d=g>20?true: false;
+// console.log(d)
+
+function greet(word){
+    alert("Hello",word);
+}
+greet("Chimo");
+
